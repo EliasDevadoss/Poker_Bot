4de9030c43a7f5f4bce21c0dcfb9bbcc91279580
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 20:59:41 -0700

    Formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from card_deck import CardDeck
 
-st.title("Poker ♠️ Bot")
+st.title("Poker ♠️ Bot.")
 st.write(
-    "Welcome to the Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee."
+    "Welcome to the minimalist Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee."
 )
 
 deck = CardDeck()
@@ -13,7 +13,7 @@ river = deck.get_river()
 hero_hand = deck.get_hero()
 villain_hand = deck.get_villain()
 
-st.header("Your Hand:")
+st.header("Your Hand.")
 col01, col02, col03 = st.columns([1, 1, 3], gap="medium", vertical_alignment="top")
 with col01:
     st.header(hero_hand[0], divider="violet")
@@ -22,7 +22,7 @@ with col02:
 
 col1, col2, col3, col4, col5 = st.columns(5, gap="medium", vertical_alignment="top")
 with col1:
-    st.header("Flop")
+    st.header("Flop.")
     st.header(flop[0], divider="violet")
 with col2:
     st.header("")
@@ -31,8 +31,8 @@ with col3:
     st.header("")
     st.header(flop[2], divider="violet")
 with col4:
-    st.header("Turn")
+    st.header("Turn.")
     st.header(turn[0], divider="violet")
 with col5:
-    st.header("River")
+    st.header("River.")
     st.header(river[0], divider="violet")
