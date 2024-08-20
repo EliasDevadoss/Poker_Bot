b125a89eebd23ed3e11711c558b7ea93f46dcd9b
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 17:34:41 -0700

    Named

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Poker ♠️ Bot")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Welcome to the Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee"
 )
