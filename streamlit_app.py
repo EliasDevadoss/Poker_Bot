669a70cf839fefaa470b91cc4ff43997584a3fbb
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 18:03:04 -0700

    First Card

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("Poker ♠️ Bot")
 st.write(
     "Welcome to the Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee"
 )
+
+st.text_area(label, value="9♠️", height=100, label_visibility="hidden")
