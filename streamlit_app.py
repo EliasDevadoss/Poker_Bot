294f06663657cd847a7487fe0c9b254b6ccf24db
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 23:30:52 -0700

    Updated card symbols

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with col1:
     if st.session_state.flop:
         st.header(flop[0], divider="violet")
     else:
-        st.header("|௹|", divider="violet")
+        st.header("|⨔|", divider="violet")
 with col2:
     st.subheader("")
     if st.session_state.flop:
@@ -48,7 +48,7 @@ with col3:
     if st.session_state.flop:
         st.header(flop[2], divider="violet")
     else:
-        st.header("|⫷|", divider="violet")
+        st.header("|⨔|", divider="violet")
 with col4:
     st.subheader("Turn")
     st.header(turn[0], divider="violet")
