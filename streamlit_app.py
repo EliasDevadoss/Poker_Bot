42272ea315e2cc583f596fb62e6ac2bf5d931185
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 18:22:00 -0700

    Removing original text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,11 @@ st.write(
     "Welcome to the Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee"
 )
 
-st.text_area("No Label", value="9♠️", height=100, key=0, label_visibility="hidden")
-
 col1, col2, col3, col4, col5 = st.columns(5, gap="medium", vertical_alignment="top")
 
 with col1:
     st.header("Flop")
-    st.text_area("No Label", value="9♠️", height=100, key=1, label_visibility="hidden")
+    st.text_area("No Label", value="9♠️", key=1, label_visibility="hidden")
 
 with col2:
     st.header("")
@@ -19,12 +17,12 @@ with col2:
 
 with col3:
     st.header("")
-    st.text_area("No Label", value="7♠️", height=100, key=3, label_visibility="hidden")
+    st.text_area("No Label", value="7♠️", key=3, label_visibility="hidden")
 
 with col4:
     st.header("Turn")
-    st.text_area("No Label", value="6♠️", height=100, key=4, label_visibility="hidden")
+    st.text_area("No Label", value="6♠️", key=4, label_visibility="hidden")
 
 with col5:
     st.header("River")
-    st.text_area("No Label", value="5♠️", height=100, key=5, label_visibility="hidden")
+    st.text_area("No Label", value="5♠️", key=5, label_visibility="hidden")
