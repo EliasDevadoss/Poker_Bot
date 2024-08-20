0fbfeb1167a403373ed398cb5241f585da9bcc91
Elias Devadoss <eliasdevad@gmail.com>
2024-08-19 18:20:13 -0700

    Keys added

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,23 @@ st.write(
     "Welcome to the Poker Bot. Designed by Elias Devadoss, Himal Pandey, and Marcus Lee"
 )
 
-st.text_area("No Label", value="9♠️", height=100, label_visibility="hidden")
+st.text_area("No Label", value="9♠️", height=100, key=0, label_visibility="hidden")
 
 col1, col2, col3, col4, col5 = st.columns(5, gap="medium", vertical_alignment="top")
 
 with col1:
     st.header("Flop")
-    st.text_area("No Label", value="9♠️", height=100, label_visibility="hidden")
+    st.text_area("No Label", value="9♠️", height=100, key=1, label_visibility="hidden")
 
 with col2:
-    st.text_area("No Label", value="8♠️", label_visibility="hidden")
+    st.text_area("No Label", value="8♠️", key=2, label_visibility="hidden")
 
 with col3:
-    st.text_area("No Label", value="7♠️", height=100, label_visibility="hidden")
+    st.text_area("No Label", value="7♠️", height=100, key=3, label_visibility="hidden")
 
 with col4:
-    st.text_area("Turn", value="6♠️", height=100, label_visibility="hidden")
+    st.text_area("Turn", value="6♠️", height=100, key=4, label_visibility="hidden")
 
 with col5:
-    st.text_area("River", value="5♠️", height=100, label_visibility="hidden")
+    st.text_area("River", value="5♠️", height=100, key=5, label_visibility="hidden")
 
